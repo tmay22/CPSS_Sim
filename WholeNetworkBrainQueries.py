2a24f293476184120164facedbf1c07d51c5a73b
Tess <ts.promo8@gmail.com>
2025-01-20 15:25:55 +1100

    Finished 1-3-2 Whole Network Standard Deviation on Topic by Num of Instances

--- WholeNetworkBrainQueries.py
+++ WholeNetworkBrainQueries.py
@@ -31,8 +31,38 @@ def getNetworkBeliefOnTopic():
     
     # CHeck and call vector function
     atomicV = Globals.atomic_VectorDictionary[wordOne]
-
     result = VectorFunction_Brain.getRankedNetworkBelief(atomicV)
 
     print(f'RESULT: Strongest Terms: {result}')
+    print("----------------------------------------")
     return
+
+def getNetworkSDBeliefOnTopic_Val():
+    # Get the  variability of the entire network's feeling on a topic
+
+    print("----------------------------------------")
+    print("What is the standard deviation range value of feelings on a topic? By number of instances (not vector values)")
+    print("----------------------------------------")
+    # COllect inputs
+    wordOne= input("Give word: ")
+    print("You input: " + wordOne)
+    
+   
+    print("----------------------------------------")
+    print(f'Checking inputs...')
+    
+    # Convert words to lower case
+    wordOne = wordOne.lower()
+
+    # General error check of inputs
+    if not Checks.checkAtomicExists(wordOne):
+        print(f'{wordOne} does not exist')
+        return
+    
+    # CHeck and call vector function
+    atomicV = Globals.atomic_VectorDictionary[wordOne]
+    result = VectorFunction_Brain.getStandardDeviationBelief(atomicV)
+    print(f'RESULT: Variance is: {result}')
+    print("----------------------------------------")
+    return
+    
