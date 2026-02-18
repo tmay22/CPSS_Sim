9abb81f0e6dee06fd3d8d11a26b103d299b045ab
Tess <ts.promo8@gmail.com>
2026-02-18 16:24:32 +1100

    Fixed bugs and commented out CPS stuff

--- Start.py
+++ Start.py
@@ -41,7 +41,7 @@ def getSimPath():
 
     if simDataOption == "1":
         #pathOne = "DataSets/20AI/"
-        path = "DataSets\\OBA\\PerplexityGen\\"
+        path = "DataSets\\InnerVsOuter\\FullCSVs\\"
         historyOption = True
     elif simDataOption == "2":
         print("Input a path, with a / at the end")
